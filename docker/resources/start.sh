eb24ad8c91efcd15f742094e2ca9a654bf3e508a
Ankur_Garg <ankurgarg1203@gmail.com>
2021-08-02 13:41:13 +0530

    updating scripts

--- docker/resources/start.sh
+++ docker/resources/start.sh
@@ -1,10 +1,9 @@
 #!/bin/sh
 
-echo -e "Installing and Running the web server\n\n"
+echo -e "\n\nInstalling and Running the web server\n\n"
 python3 server.py &
 
-echo -e "Sleeping for 5 seconds for web server to be up...\n\n"
 sleep 5
 
-echo -e "Now executing test cases...\n\n"
+echo -e "\n\nNow executing test cases...\n\n"
 python3 test.py
--- docker/resources/test.py
+++ docker/resources/test.py
@@ -8,12 +8,12 @@ def testServerResponse():
         print("General exception !! Please check the server configuration",e )   
 
     if(response.status_code == 200):
-        print("Test 1: PASSED -- Status Code Test. Received required status code "+str(response.status_code))
+        print("\nTest 1: PASSED -- Status Code Test. Received required status code "+str(response.status_code))
     else:
-        print("Test 1: FAILED -- Status Code Test. Received status code: "+str(response.status_code)+ " instead of 200")
+        print("\nTest 1: FAILED -- Status Code Test. Received status code: "+str(response.status_code)+ " instead of 200")
     if(response.json() == {"myFavouriteTree":"tulsi"}):
-        print("Test 2: PASSED -- Content Verfication Test. Received required response: "+str(response.json()))
+        print("\nTest 2: PASSED -- Content Verfication Test. Received required response: "+str(response.json()))
     else:
-        print("Test 2: FAILED -- Content Verification Test. Required content: {'myFavouriteTree': 'tulsi'} but Received response is: "+str(response.json()))        
+        print("\nTest 2: FAILED -- Content Verification Test. Required content: {'myFavouriteTree': 'tulsi'} but Received response is: "+str(response.json()))        
 
 testServerResponse()
