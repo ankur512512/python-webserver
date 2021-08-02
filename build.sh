096fbc06f6d1eeb60c5d10f89eeac6c41476ee0e
Ankur_Garg <ankurgarg1203@gmail.com>
2021-08-02 14:27:30 +0530

    more readable

--- build.sh
+++ build.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 
-echo -e "\nEnabling ingress for minikube\n"
+echo -e "\n***Enabling ingress for minikube\n"
 
 minikube addons enable ingress
 
-echo -e "\nWaiting for ingress-controller to get ready, please give it a couple of minutes...\n"
+echo -e "\n***Waiting for ingress-controller to get ready, please give it a couple of minutes...\n"
 
 kubectl wait --timeout=120s --for=condition=ready pod -n kube-system -l app.kubernetes.io/name=nginx-ingress-controller
 
-echo -e "\nBuilding the docker image locally\n"
+echo -e "\n***Building the docker image locally\n"
 
 docker build -t ecosia:latest docker/.
 
-echo -e "\nApplying kubernetes manifests from yaml files store in templates folder...\n"
+echo -e "\n***Applying kubernetes manifests from yaml files store in templates folder...\n"
 
 kubectl apply -f templates/
 
@@ -20,14 +20,14 @@ sleep 7
 
 POD=$(kubectl get pod -l app=flask -o jsonpath="{.items[0].metadata.name}")
 
-echo -e "\nPrinting container logs showing server logs and local testing."
+echo -e "\n***Printing container logs showing server logs and local testing."
 kubectl logs $POD 
 
-echo -e "\nAdding host file entry in /etc/hosts..."
+echo -e "\n***Adding host file entry in /etc/hosts..."
 
 echo "`minikube ip`  local.ecosia.org" >> /etc/hosts
 
-echo -e "\nHost entry added. Now testing to see if we get required response with curl command for url -- curl https://local.ecosia.org Here's the result: \n\n"
+echo -e "\n***Host entry added. Now testing to see if we get required response with curl command for url -- curl https://local.ecosia.org Here's the result: \n\n"
 
 curl http://local.ecosia.org/tree
 
