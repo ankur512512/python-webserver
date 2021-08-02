375c1825ba3a05eb19f5c045564e8cb2467f1979
Ankur_Garg <ankurgarg1203@gmail.com>
2021-08-02 13:37:05 +0530

    updating script

--- docker/resources/start.sh
+++ docker/resources/start.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
 
+echo -e "Installing and Running the web server\n\n"
 python3 server.py &
+
+echo -e "Sleeping for 5 seconds for web server to be up...\n\n"
 sleep 5
+
+echo -e "Now executing test cases...\n\n"
 python3 test.py
